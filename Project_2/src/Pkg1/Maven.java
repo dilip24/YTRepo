c90052078acd394f0e4bd9db4142985b866ddda9
lenovo <lenovo@user>
2022-05-16 18:41:30 +0530

    Pushing code for testing

---
+++ Project_2/src/Pkg1/Maven.java
@@ -0,0 +1,13 @@
+package Pkg1;
+
+import Day1.Student;
+
+public class Maven 
+{
+	public static void main(String[] args)
+	{
+		Student ob = new Student();
+		ob.display1();
+		ob.display2();
+	}
+}
